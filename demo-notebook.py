5a588e2b1562b87939580c2f170beca03700f81d
AurDataAnalyst <aurelectronic@gmail.com>
2020-02-28 18:35:16 +0100

    Farmer_Market_Study

---
+++ demo-notebook.py
@@ -0,0 +1,19 @@
+# Databricks notebook source
+sc
+
+# COMMAND ----------
+
+sqlContext
+
+# COMMAND ----------
+
+# DBTITLE 1,Topic for the page 
+# MAGIC %md This is the Spark context for Spark 2.0 or greater
+
+# COMMAND ----------
+
+# MAGIC %md This is some more text
+
+# COMMAND ----------
+spark
+
